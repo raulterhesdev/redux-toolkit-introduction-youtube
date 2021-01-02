81414928a41e95dfed711c7c1df873eaa6907b91
Raul Terhes <raulterhesdevelopment@gmail.com>
2021-01-02 15:30:35 +0200

    added actions + example action type & action creator

--- src/store/dummyReducer.js
+++ src/store/dummyReducer.js
@@ -1,3 +1,13 @@
+import { addBook } from './actions/booksActions';
+
+console.log(addBook.toString());
+
+console.log(`Action type: ${addBook}`);
+
+console.log(addBook());
+
+console.log(addBook('Book'));
+
 // shall be deleted when the real reducers are created
 const initialState = { test: 'It works' };
 
