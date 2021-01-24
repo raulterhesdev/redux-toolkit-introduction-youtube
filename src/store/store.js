effb4821d3d55be8810f8950f3de7f9ce12cf04a
Raul Terhes <raulterhesdevelopment@gmail.com>
2021-01-24 11:54:47 +0200

    replaced redux-thunk with redux-saga when creating the store

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 // import booksReducer from './reducers/booksReducer';
 import booksReducer from './slices/bookSlice';
 import authReducer from './slices/authSlice';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+
+// const store = configureStore({
+//   reducer: { books: booksReducer, auth: authReducer },
+// });
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: { books: booksReducer, auth: authReducer },
+  reducer: { books: booksReducer },
+  // middleware: [sagaMiddleware]
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
